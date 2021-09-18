2bb64ba66be4ced4409a2b41888ab4e5f1b62947
hantao <hantao@butel.com>
2021-09-18 15:59:43 +0800

    删掉拼接构建时间的宏，相关的宏都不可用

--- cmake/generate_version_file.cmake
+++ cmake/generate_version_file.cmake
@@ -1,10 +1,10 @@
 
-include(cmake/get_git_info.cmake)
+include(${CMAKE_SOURCE_DIR}/cmake/get_git_info.cmake)
 
 set(VERSION_FILE "version.h")
 
 # message(STATUS "Generate ${VERSION_FILE} file")
 string(TOUPPER ${PROJECT_NAME} UPPERCASE_PROJECT_NAME)
 
-configure_file(./version.h.in ${CMAKE_SOURCE_DIR}/version.h @ONLY NEWLINE_STYLE LF)
+configure_file(${CMAKE_SOURCE_DIR}/version.h.in ${CMAKE_SOURCE_DIR}/version.h @ONLY NEWLINE_STYLE LF)
 message(STATUS "Generate file to ${CMAKE_SOURCE_DIR}/version.h")
---
+++ test/generated_version_file/test_generated_version_file.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+#include "version.h"
+
+void ShowVersionInfo(void) {
+    std::cout << REPOSITORY << std::endl;
+    std::cout << "0.1.0." << REVISION_NUM << std::endl;
+    std::cout << "build time: " <<
+        BUILD_YEAR_CH0 << BUILD_YEAR_CH1 <<
+        BUILD_YEAR_CH2 << BUILD_YEAR_CH3 <<
+        '-' << BUILD_MONTH_CH0 << BUILD_MONTH_CH1 <<
+        '-' << BUILD_DAY_CH0 << BUILD_DAY_CH1 <<
+        ' ' << __TIME__<< std::endl;
+    std::cout << BUILD_TIME_STR2 << std::endl;
+    //std::cout << BUILD_TIME_STR3 << std::endl;
+}
+
+int main(int argc, char** argv) {
+    ShowVersionInfo();
+
+    return 0;
+}
