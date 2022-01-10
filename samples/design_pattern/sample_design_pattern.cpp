a4bd07489ea1568c80fed94dd4fa6d3dbcff3558
unknown <coederqs@gmail.com>
2022-01-10 14:17:43 +0800

    修改 sample_design_pattern 的作用

--- samples/design_pattern/sample_design_pattern.cpp
+++ samples/design_pattern/sample_design_pattern.cpp
@@ -1,16 +1,18 @@
 #include <iostream>
-#include "DesignPattern/Singleton.h"
-
-class SampleSingleton : public Singleton<SampleSingleton> {
-public:
-    SampleSingleton() = default;
-    ~SampleSingleton() = default;
-};
+#include "sample_dp_sigleton.h"
+#include "sample_dp_factory.h"
 
 int main(int argc, char** argv) {
-    SampleSingleton* s1 = SampleSingleton::GetInstance();
-    SampleSingleton* s2 = SampleSingleton::GetInstance();
-    std::cout << "s1 = " << s1 << std::endl <<
-        "s2 = " << s2 << std::endl;
+    // sample singleton
+    //SampleSingleton* s1 = SampleSingleton::GetInstance();
+    //SampleSingleton* s2 = SampleSingleton::GetInstance();
+    //std::cout << "s1 = " << s1 << std::endl <<
+    //    "s2 = " << s2 << std::endl;
+
+    // abstract factory 
+    Factory factory;
+    ProductA* product_a = (ProductA*)factory.Create("ProductA");
+    ProductB* product_b = (ProductB*)factory.Create("ProductB");
+
     return 0;
 }
