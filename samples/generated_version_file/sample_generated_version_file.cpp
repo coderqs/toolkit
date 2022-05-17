74213b4b1789ae50a09ba8e2b4bcbd4154ff8724
coderqs <coderqs@coderqs.com>
2022-05-18 00:39:01 +0800

    更新例子

--- samples/generated_version_file/sample_generated_version_file.cpp
+++ samples/generated_version_file/sample_generated_version_file.cpp
@@ -2,18 +2,18 @@
 
 #include "version.h"
 
-void ShowVersionInfo(void) {
-    std::cout << REPOSITORY << std::endl;
-    std::cout << "0.1.0." << REVISION_NUM << std::endl;
-    std::cout << "build time: " <<
-        BUILD_YEAR_CH0 << BUILD_YEAR_CH1 <<
-        BUILD_YEAR_CH2 << BUILD_YEAR_CH3 <<
-        '-' << BUILD_MONTH_CH0 << BUILD_MONTH_CH1 <<
-        '-' << BUILD_DAY_CH0 << BUILD_DAY_CH1 <<
-        ' ' << __TIME__<< std::endl;
-    //std::cout << BUILD_TIME_STR2 << std::endl;
-    //std::cout << BUILD_TIME_STR3 << std::endl;
-}
+//void ShowVersionInfo(void) {
+//    std::cout << REPOSITORY << std::endl;
+//    std::cout << "0.1.0." << REVISION_NUM << std::endl;
+//    std::cout << "build time: " <<
+//        BUILD_YEAR_CH0 << BUILD_YEAR_CH1 <<
+//        BUILD_YEAR_CH2 << BUILD_YEAR_CH3 <<
+//        '-' << BUILD_MONTH_CH0 << BUILD_MONTH_CH1 <<
+//        '-' << BUILD_DAY_CH0 << BUILD_DAY_CH1 <<
+//        ' ' << __TIME__<< std::endl;
+//    //std::cout << BUILD_TIME_STR2 << std::endl;
+//    //std::cout << BUILD_TIME_STR3 << std::endl;
+//}
 
 int main(int argc, char** argv) {
     ShowVersionInfo();
