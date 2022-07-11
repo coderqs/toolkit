ab7ae50c158f58eaab92a5f52b78106864fd0b8d
coderqs <coderqs@coderqs.com>
2022-07-12 01:44:58 +0800

    修改加载 ColorEcho.bash 的保障机制

--- scripts/general/clog_print.sh
+++ scripts/general/clog_print.sh
@@ -3,8 +3,14 @@
 script_abs=$(readlink -f "$0")
 script_dir=$(dirname $script_abs)
 
-#source ${script_dir}/../../3rdparty/ColorEchoForShell/dist/ColorEcho.sh
-source ../../3rdparty/ColorEchoForShell/dist/ColorEcho.bash
+source ${script_dir}/../../3rdparty/ColorEchoForShell/dist/ColorEcho.bash
+if [ $? ];then
+    wget https://raw.githubusercontent.com/PeterDaveHello/ColorEchoForShell/master/dist/ColorEcho.bash
+    source ColorEcho.bash
+    if [ $? ];then
+            echo "[Error] load ColorEcho.bash failure!" && exit 1 
+        fi
+fi
 
 function LOG_ERROR() {
     echo.BoldRed $*
